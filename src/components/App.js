3cd92e8e97bc516a8a0969a99353142ec84fec06
TusharMinche <tusharminche@gmail.com>
2025-09-14 00:42:55 +0530

    trim function removed

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,15 @@ const App = () => {
 
   const handleInput = (e) => {
     setName(e.target.value);
-  }
+  };
 
   return (
     <div>
-      <label htmlFor="username">Enter your name: </label>
+      <label htmlFor="username">Enter your name:</label>
       <input id="username" type="text" onChange={handleInput} />
-      <p>{name === '' ? '' : `Hello, ${name}!`}</p>
+      <p>{name ? `Hello, ${name}!` : ''}</p>
     </div>
   );
-}
+};
 
 export default App;
